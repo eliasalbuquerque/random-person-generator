fbe5646f9ba7ef2848ce11cedaa983b93dbc56ad
Elias Albuquerque <78819295+eliasalbuquerque@users.noreply.github.com>
2024-09-28 18:22:29 -0300

    feat(app): update of input functions, generate and save data
    ADDED FEATURES:
    
    * implements functions to obtain and process user input
    * implements function to generate people from the Person class
    * implements saving files in json, xlsx and txt formats
    
    BUG FIXES:
    
    * fixed the bug of saving files in xlsx using the pandas library

--- app.py
+++ app.py
@@ -2,90 +2,99 @@
 # author: 'Elias Albuquerque'
 # version: '0.1'
 # created: '2024-09-27'
-# update: '2024-09-27'
+# update: '2024-09-28'
 
 
-from faker import Faker
-import re
+from src.pessoa import Pessoa
 import json
 import pandas as pd
 
-class Pessoa:
-    def __init__(self):
-        self.fake = Faker('pt_BR')
 
-    def gerar_dados_pessoa(self):
-        """Gera dados fictícios de uma pessoa."""
+def obter_quantidade():
+    """Solicita e valida a entrada para a quantidade de pessoas a serem geradas."""
 
-        nome = self.fake.name()
-        cpf = self.gerar_cpf_valido()
-        email = self.fake.email()
-        endereco = self.fake.address()
-        telefone = self.fake.phone_number()
-        
-        return {
-            'nome': nome,
-            'cpf': cpf,
-            'email': email,
-            'endereco': endereco,
-            'telefone': telefone
-        }
+    while True:
+        try:
+            quantidade = int(input("Digite a quantidade de pessoas a serem geradas: "))
+            if quantidade <= 0:
+                print("Erro: A quantidade deve ser um número positivo.\n")
+            else:
+                return quantidade
+        except ValueError:
+            print("Erro: Por favor, digite um número inteiro válido.\n")
 
-    def gerar_cpf_valido(self):
-        """Gera um CPF válido, mas falso, utilizando a biblioteca Faker e verificações de dígitos."""
 
-        cpf_base = self.fake.numerify('#########')  # Gera uma string de 9 dígitos aleatórios
+def obter_formato():
+    """Solicita e valida o formato de saída (json , xlsx, txt)."""
 
-        # Calcula o primeiro dígito verificador
-        soma = sum([int(digito) * (10 - i) for i, digito in enumerate(cpf_base)])
-        resto = 11 - (soma % 11)
-        digito1 = resto if resto <= 9 else 0
+    formatos_validos = ["json", "xlsx", "txt", "j", "x", "t"]
 
-        # Calcula o segundo dígito verificador
-        soma = sum([int(digito) * (11 - i) for i, digito in enumerate(cpf_base + str(digito1))])
-        resto = 11 - (soma % 11)
-        digito2 = resto if resto <= 9 else 0
-
-        cpf_completo = cpf_base + str(digito1) + str(digito2)
-
-        # Formata o CPF
-        cpf_formatado = re.sub(r'(\d{3})(\d{3})(\d{3})(\d{2})', r'\1.\2.\3-\4', cpf_completo)
-
-        return cpf_formatado
+    while True:
+        formato = input("Digite o formato de saída ([j] json, [x] xlsx, [t] txt): ").lower()
+        if formato not in formatos_validos:
+            print(f"Erro: Formato inválido! Os formatos aceitos são: json, xls, txt.\n")
+        else:
+            return formato
 
 def gerar_pessoas(quantidade):
     """Gera uma lista de dicionários com dados de pessoas."""
+
     pessoas = []
+
     for _ in range(quantidade):
         pessoa = Pessoa().gerar_dados_pessoa()
         pessoas.append(pessoa)
+
     return pessoas
+
 
 def salvar_dados(pessoas, formato):
     """Salva os dados gerados no formato escolhido."""
 
-    if formato == "json":
+    if formato == "json" or formato == "j":
         with open("pessoas.json", "w", encoding='utf-8') as f:
-            json.dump(pessoas, f, indent=4)
+            json.dump(pessoas, f, indent=4, ensure_ascii=False)
         print("Dados salvos em pessoas.json")
-    elif formato == "xlsx":
+
+    elif formato == "xlsx" or formato == "x":
+        # Criar DataFrame e salvar em Excel
         df = pd.DataFrame(pessoas)
-        df.to_excel("pessoas.xlsx", index=False)
+        with pd.ExcelWriter("pessoas.xlsx", engine='xlsxwriter') as writer:
+            df.to_excel(writer, sheet_name='Dados', index=False)
+
+            # Pegar o objeto worksheet para ajustar colunas
+            worksheet = writer.sheets['Dados']
+            
+            # Ajustar a largura das colunas com base nos conteúdos
+            for idx, col in enumerate(df.columns):
+                max_len = max(df[col].astype(str).map(len).max(), len(col))
+                worksheet.set_column(idx, idx, max_len)
+
         print("Dados salvos em pessoas.xlsx")
-    elif formato == "txt":
+
+    elif formato == "txt" or formato == "t":
         with open("pessoas.txt", "w", encoding='utf-8') as f:
             for pessoa in pessoas:
                 f.write(f"Nome: {pessoa['nome']}\n")
                 f.write(f"CPF: {pessoa['cpf']}\n")
                 f.write(f"Email: {pessoa['email']}\n")
                 f.write(f"Endereço: {pessoa['endereco']}\n")
-                f.write(f"Telefone: {pessoa['telefone']}\n\n")
+                f.write(f"Contato: {pessoa['contato']}\n")
         print("Dados salvos em pessoas.txt")
+
     else:
-        print("Formato inválido!")
+        print("Formato inválido!\n")
+
 
 if __name__ == "__main__":
-    quantidade = int(input("Digite a quantidade de pessoas a serem geradas: "))
-    formato = input("Digite o formato de saída (json, xlsx, txt): ")
-    pessoas = gerar_pessoas(quantidade)
-    salvar_dados(pessoas, formato)
+    try:
+        # Input usuário
+        quantidade = obter_quantidade()
+        formato = obter_formato()
+
+        # Gera e salva os dados conforme entradas validadas
+        pessoas = gerar_pessoas(quantidade)
+        salvar_dados(pessoas, formato)
+
+    except KeyboardInterrupt:
+        print("\nExecução interrompida pelo usuário. Finalizando o programa...\n")
